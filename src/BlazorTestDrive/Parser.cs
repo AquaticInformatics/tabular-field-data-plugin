1da9bbde04f6efabd951f72b128575ff30ab991b
Doug Schmidt <doug.schmidt@aquaticinformatics.com>
2020-06-22 18:51:06 -0700

    PF-670 - First cut of Blazor demo app

---
+++ src/BlazorTestDrive/Parser.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Text.Json;
+using FieldDataPluginFramework;
+using FieldDataPluginFramework.Context;
+using FieldDataPluginFramework.DataModel;
+using FieldDataPluginFramework.DataModel.Calibrations;
+using FieldDataPluginFramework.DataModel.ControlConditions;
+using FieldDataPluginFramework.DataModel.CrossSection;
+using FieldDataPluginFramework.DataModel.DischargeActivities;
+using FieldDataPluginFramework.DataModel.Inspections;
+using FieldDataPluginFramework.DataModel.LevelSurveys;
+using FieldDataPluginFramework.DataModel.Readings;
+using FieldDataPluginFramework.Results;
+
+namespace BlazorTestDrive
+{
+    public class Parser
+    {
+        public FakeLogger Logger { get; } = new FakeLogger();
+
+        public (string Output, string Details, Results Results) Parse(string config, string csv)
+        {
+            var plugin = LoadTabularPlugin();
+
+            var appender = new FakeAppender();
+
+            using (var stream = new MemoryStream(Encoding.UTF8.GetBytes(csv)))
+            {
+                WriteTemporaryConfigFile(config);
+
+                var result = plugin.ParseFile(stream, appender, Logger);
+
+                var options = new JsonSerializerOptions
+                {
+                    WriteIndented = true
+                };
+
+                if (result.Status == ParseFileStatus.SuccessfullyParsedAndDataValid)
+                {
+                    Logger.Info($"{result.Status}");
+                }
+                else
+                {
+                    Logger.Error($"{result.Status}: Parsed={result.Parsed} ErrorMessage={result.ErrorMessage}");
+                }
+
+                appender.AppendedResults.PluginAssemblyQualifiedTypeName = plugin.GetType().AssemblyQualifiedName;
+
+                var results = Results.CreateResults(appender.AppendedResults);
+
+                var json = JsonSerializer.Serialize(appender.AppendedResults, options);
+                return (json, Logger.Builder.ToString(), results);
+            }
+        }
+
+        private void WriteTemporaryConfigFile(string configToml)
+        {
+            var path = Path.Combine(
+                Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData),
+                @"Aquatic Informatics\AQUARIUS Server\Configuration\TabularCSV",
+                "config.toml");
+
+            var fileInfo = new FileInfo(path);
+
+            // ReSharper disable once AssignNullToNotNullAttribute
+            Directory.CreateDirectory(fileInfo.DirectoryName);
+
+            File.WriteAllText(path, configToml);
+        }
+
+        private IFieldDataPlugin LoadTabularPlugin()
+        {
+            return new TabularCsv.Plugin();
+        }
+    }
+
+    public class FakeLogger : ILog
+    {
+        public StringBuilder Builder = new StringBuilder();
+
+        public void Info(string message)
+        {
+            Builder.AppendLine($"INFO: {message}");
+        }
+
+        public void Error(string message)
+        {
+            Builder.AppendLine($"ERROR: {message}");
+        }
+    }
+
+    public class InternalConstructor<T> where T : class
+    {
+        public static T Invoke(params object[] args)
+        {
+            return Activator.CreateInstance(typeof(T), BindingFlags.NonPublic | BindingFlags.Instance, null, args, null) as T;
+        }
+    }
+
+    public class AppendedResults
+    {
+        public string FrameworkAssemblyQualifiedName { get; set; }
+        public string PluginAssemblyQualifiedTypeName { get; set; }
+        public List<FieldVisitInfo> AppendedVisits { get; set; } = new List<FieldVisitInfo>();
+    }
+
+    public class FakeAppender : IFieldDataResultsAppender
+    {
+        public LocationInfo CreateDummyLocationInfoByIdentifier(string locationIdentifier)
+        {
+            return CreateDummyLocationInfo(locationIdentifier, $"DummyNameFor-{locationIdentifier}", Guid.Empty);
+        }
+
+        private LocationInfo CreateDummyLocationInfoByUniqueId(Guid uniqueId)
+        {
+            return CreateDummyLocationInfo($"DummyIdentifierFor-{uniqueId:N}", $"DummyNameFor-{uniqueId:N}", uniqueId);
+        }
+
+        private LocationInfo CreateDummyLocationInfo(string identifier, string name, Guid uniqueId)
+        {
+            const long dummyLocationId = 0;
+
+            var locationInfo = InternalConstructor<LocationInfo>.Invoke(
+                name,
+                identifier,
+                dummyLocationId,
+                uniqueId,
+                UtcOffset.TotalHours);
+
+            if (KnownLocations.Any(l => l.LocationIdentifier == identifier))
+                throw new ArgumentException($"Can't add duplicate location for Identifier='{identifier}'");
+
+            KnownLocations.Add(locationInfo);
+
+            return locationInfo;
+        }
+
+        private static readonly List<LocationInfo> KnownLocations = new List<LocationInfo>();
+
+        public LocationInfo ForcedLocationInfo { get; set; }
+        public TimeSpan UtcOffset { get; set; }
+
+        public AppendedResults AppendedResults { get; } = new AppendedResults
+        {
+            FrameworkAssemblyQualifiedName = typeof(IFieldDataPlugin).AssemblyQualifiedName
+        };
+
+        public LocationInfo GetLocationByIdentifier(string locationIdentifier)
+        {
+            if (ForcedLocationInfo != null)
+            {
+                if (ForcedLocationInfo.LocationIdentifier == locationIdentifier)
+                    return ForcedLocationInfo;
+
+                throw new ArgumentException($"Location {locationIdentifier} does not exist");
+            }
+
+            var locationInfo = KnownLocations.SingleOrDefault(l => l.LocationIdentifier == locationIdentifier);
+
+            return locationInfo ?? CreateDummyLocationInfoByIdentifier(locationIdentifier);
+        }
+
+        public LocationInfo GetLocationByUniqueId(string uniqueIdText)
+        {
+            if (!Guid.TryParse(uniqueIdText, out var uniqueId))
+                throw new ArgumentException($"Can't parse '{uniqueIdText}' as a unique ID");
+
+            var locationInfo = KnownLocations.SingleOrDefault(l => Guid.Parse(l.UniqueId) == uniqueId);
+
+            return locationInfo ?? CreateDummyLocationInfoByUniqueId(uniqueId);
+        }
+
+        public FieldVisitInfo AddFieldVisit(LocationInfo location, FieldVisitDetails fieldVisitDetails)
+        {
+            var fieldVisitInfo = InternalConstructor<FieldVisitInfo>.Invoke(location, fieldVisitDetails);
+
+            AppendedResults.AppendedVisits.Add(fieldVisitInfo);
+
+            return fieldVisitInfo;
+        }
+
+        public void AddDischargeActivity(FieldVisitInfo fieldVisit, DischargeActivity dischargeActivity)
+        {
+            fieldVisit.DischargeActivities.Add(dischargeActivity);
+        }
+
+        public void AddControlCondition(FieldVisitInfo fieldVisit, ControlCondition controlCondition)
+        {
+            fieldVisit.ControlConditions.Add(controlCondition);
+        }
+
+        public void AddCrossSectionSurvey(FieldVisitInfo fieldVisit, CrossSectionSurvey crossSectionSurvey)
+        {
+            fieldVisit.CrossSectionSurveys.Add(crossSectionSurvey);
+        }
+
+        public void AddReading(FieldVisitInfo fieldVisit, Reading reading)
+        {
+            fieldVisit.Readings.Add(reading);
+        }
+
+        public void AddCalibration(FieldVisitInfo fieldVisit, Calibration calibration)
+        {
+            fieldVisit.Calibrations.Add(calibration);
+        }
+
+        public void AddInspection(FieldVisitInfo fieldVisit, Inspection inspection)
+        {
+            fieldVisit.Inspections.Add(inspection);
+        }
+
+        public void AddLevelSurvey(FieldVisitInfo fieldVisit, LevelSurvey levelSurvey)
+        {
+            fieldVisit.LevelSurveys.Add(levelSurvey);
+        }
+    }
+}
---
+++ src/BlazorTestDrive/Results.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using FieldDataPluginFramework.Context;
+using FieldDataPluginFramework.DataModel.Calibrations;
+using FieldDataPluginFramework.DataModel.ControlConditions;
+using FieldDataPluginFramework.DataModel.DischargeActivities;
+using FieldDataPluginFramework.DataModel.Inspections;
+using FieldDataPluginFramework.DataModel.LevelSurveys;
+using FieldDataPluginFramework.DataModel.Readings;
+
+namespace BlazorTestDrive
+{
+    public class Results
+    {
+        public List<FieldVisitInfo> Visits { get; } = new List<FieldVisitInfo>();
+        public List<(string VisitIdentifier, Reading Item)> Readings { get; } = new List<(string VisitIdentifier, Reading Item)>();
+        public List<(string VisitIdentifier, Inspection Item)> Inspections { get; } = new List<(string VisitIdentifier, Inspection Item)>();
+        public List<(string VisitIdentifier, Calibration Item)> Calibrations { get; } = new List<(string VisitIdentifier, Calibration Item)>();
+        public List<(string VisitIdentifier, ControlCondition Item)> ControlConditions { get; } = new List<(string VisitIdentifier, ControlCondition Item)>();
+        public List<(string VisitIdentifier, DischargeActivity Item)> Discharges { get; } = new List<(string VisitIdentifier, DischargeActivity Item)>();
+        public List<(string VisitIdentifier, LevelSurvey Item)> LevelSurveys { get; } = new List<(string VisitIdentifier, LevelSurvey Item)>();
+
+        public static Results CreateResults(AppendedResults appendedResults)
+        {
+            return new Results(appendedResults);
+        }
+
+        private Results(AppendedResults appendedResults)
+        {
+            var visits = appendedResults.AppendedVisits;
+
+            Visits.AddRange(visits);
+
+            Readings.AddRange(visits
+                .SelectMany(v => v.Readings.Select(item => (v.FieldVisitIdentifier, item))));
+
+            Inspections.AddRange(visits
+                .SelectMany(v => v.Inspections.Select(item => (v.FieldVisitIdentifier, item))));
+
+            Calibrations.AddRange(visits
+                .SelectMany(v => v.Calibrations.Select(item => (v.FieldVisitIdentifier, item))));
+
+            ControlConditions.AddRange(visits
+                .SelectMany(v => v.ControlConditions.Select(item => (v.FieldVisitIdentifier, item))));
+
+            Discharges.AddRange(visits
+                .SelectMany(v => v.DischargeActivities.Select(item => (v.FieldVisitIdentifier, item))));
+
+            LevelSurveys.AddRange(visits
+                .SelectMany(v => v.LevelSurveys.Select(item => (v.FieldVisitIdentifier, item))));
+        }
+    }
+}
