d882807cf1f28dbd17c0603d4e76e63ba236f73c
Doug Schmidt <doug.schmidt@aquaticinformatics.com>
2021-08-16 17:04:24 -0700

    PF-1137 - Updated test drive page to .NET 5.0

--- src/BlazorTestDrive/Program.cs
+++ src/BlazorTestDrive/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Net.Http;
 using System.Threading.Tasks;
 using Blazored.LocalStorage;
@@ -12,11 +12,11 @@ namespace BlazorTestDrive
         public static async Task Main(string[] args)
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
-            builder.RootComponents.Add<App>("app");
+            builder.RootComponents.Add<App>("#app");
 
             builder
                 .Services
-                .AddScoped(sp => new HttpClient {BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)})
+                .AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) })
                 .AddBlazoredLocalStorage(config => config.JsonSerializerOptions.WriteIndented = true);
 
             await builder.Build().RunAsync();
