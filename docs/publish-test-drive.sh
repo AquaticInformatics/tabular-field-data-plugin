f24691fb1ac1b5960581e7a51da2cc0b4995cdd9
Doug Schmidt <doug.schmidt@aquaticinformatics.com>
2022-05-17 13:42:43 -0700

    PF-1419 - Simplify the publishing script

--- docs/publish-test-drive.sh
+++ docs/publish-test-drive.sh
@@ -8,18 +8,8 @@ rm -Rf test-drive
 mkdir -p test-drive
 
 echo "* binary" > test-drive/.gitattributes
+
+dotnet publish ../src/BlazorTestDrive/BlazorTestDrive.csproj -c Release -o release --nologo
+
+cp -R release/wwwroot/* test-drive
 touch test-drive/.nojekyll
-
-cp -R ../src/BlazorTestDrive/bin/Release/net5.0/browser-wasm/publish/wwwroot/* test-drive
-
-# Rename the _framework directory to lose the leading underscore, which confuses Jekyll even when you tell it not to be confused.
-mv test-drive/_framework test-drive/framework
-
-pushd test-drive/framework
-# There are two Javascript files which contain the leading underscore as well.
-# Use sed to correct those.
-sed -e "s/_framework/framework/g" -i blazor.webassembly.js
-cd ..
-# Also correct the link in the main page
-sed -e "s/_framework/framework/g" -i index.html
-popd
